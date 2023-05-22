6fc4b6de444018cd0d17bdce08bc109091d9a5d0
abas-dev <abasisaac22@gmail.com>
2023-05-22 09:52:20 +0100

    seprated it to classes

---
+++ test project/dateWork.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+import calendar
+monthNum = datetime.now().month
+year = str(datetime.now().year)
+month = calendar.month_name[monthNum]
+day = str(datetime.now().day)
+monthYear =month,year
+monthDayYear=month,day+',',year
+current_time = datetime.now().strftime("%I:%M %p")
+time=current_time
